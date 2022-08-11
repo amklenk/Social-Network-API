51302c4deec48cfdd99c4771728628d98bbdb043
Amanda Klenk <amandamklenk3@gmail.com>
2022-08-11 13:24:58 -0400

    set up more routes in thoughts-controller (not reactions yet)

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -45,6 +45,39 @@ const thoughtController = {
             res.json(dbUserData);
         }).catch(err => res.json(err));
     },
+    // PUT /api/thoughts/thoughtsId#
+    // TODO: I think you need the user here
+    updateThought({ params, body }, res) {
+        Thought.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+        .then(dbThoughtData => {
+          if (!dbThoughtData) {
+            res.status(404).json({ message: 'No thought found with this id!' });
+            return;
+          }
+          res.json(dbThoughtData);
+        })
+        .catch(err => res.status(400).json(err));
+      },
+    //   DELETE /api/thoughts/thoughtId#
+      removeThought({ params }, res){
+        Thought.findOneAndDelete({ _id: params.thoughtId }
+            ).then(deletedThought => {
+                if(!deletedThought){
+                    return res.status(404).json({ message: 'No thought with this id!' });
+                }
+                return User.findOneAndUpdate(
+                    { _id: params.userId },
+                    { $pull: { comments: params.userId } },
+                    { new: true }
+                ).then(dbUserData => {
+                if(!dbUserData) {
+                    res.status(404).json({ message: 'No user found with this id!' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            }).catch(err => res.json(err));
+    }
 };
 
 module.exports = thoughtController;
